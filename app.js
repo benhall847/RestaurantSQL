23816403f02d3df2bccb657e4e3366953e0b4d67
Benjamin Hallquist <benhall84707@gmail.com>
2019-04-01 16:30:17 -0400

    moved handlers function to seperate file

--- app.js
+++ app.js
@@ -1,56 +1,24 @@
 const port = 1337;
-const User = require('./models/user');
 const express = require('express');
 const app = express();
-var multer = require('multer');
-var upload = multer();
-
-const Restaurant = require('./models/restaurants');
+const methods = require('./handlers');
+const {getRestaurants, getUsers, getUserById, createUser, updateUser, deleteUser} = methods;
 
 app.use(express.urlencoded({extended:true}));
 
-async function GetRestaurants (req,res){
-    res.json(await Restaurant.getAll())
-}
 
-async function getUsers (req,res){
-    res.json(await User.getAll())
-}
+app.get('/restaurants', getRestaurants);
 
-async function getUserById(req,res){
-    res.json(await User.getById(req.params.id));
-}
+app.get('/users', getUsers);
 
+app.get('/users/:id', getUserById);
 
-app.get('/restaurants', GetRestaurants)
+app.post('/users', createUser);
 
-app.get('/users', getUsers)
+app.put('/users/:id', updateUser);
 
-app.get('/users/:id', getUserById)
+app.delete('/users/:id', deleteUser);
 
-app.post('/users', async (req, res) => {
-    const {first_name, last_name, email, password} = req.body;
-
-    await User.add(first_name, last_name, email, password)
-    res.send(`added a new user ${req.body}`)
-});
-
-// app.post('/users', async(req,res) => {
-//     req.on('end',()=>{
-//         const parsedBody = querystring.parse(body)
-//         console.log(parsedBody)
-//     })
-//     res.send("You added a user!")
-// })
-
-app.put('/users/:id', async (req, res) => {
-    res.send(`You updated user: ${req.params.id}`)
-})
-app.delete('/users/:id', async (req, res) => {
-    const {id} = req.params;
-    await User.delete(id);
-    res.send(`You deleted user: ${id}`);
-})
 app.listen(port, ()=>{
     console.log(`LISTENING ON PORT ${port}`)
 })
