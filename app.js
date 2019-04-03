9cd6a2169fe719962a075ede168efd5c1fd1aeaa
Benjamin Hallquist <benhall84707@gmail.com>
2019-04-03 10:44:12 -0400

    added a bootstrap render via es6 templating

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ const port = 1337;
 const express = require('express');
 const app = express();
 const handlers = require('./handlers');
+const axios = require('axios');
 const {getRestaurants, getUsers, getUserById, createUser, updateUser, deleteUser} = handlers;
+const es6Renderer = require('express-es6-template-engine');
+
+
+app.engine('html',es6Renderer);
+app.set('view engine', 'html');
+
+app.set('views', 'views');
+// set the "views" to the "views" folder
+
+
+app.get('/login', (req,res)=>{
+    res.render('login-form')
+})
+
+
+
+app.get('/fetchme',async (req, res)=>{
+    const URL = 'http://yerkee.com/api/fortune';
+    // axios.get(URL)
+    res.send((await axios.get(URL)).data)
+})
 
 app.use(express.urlencoded({extended:true}));
-
 
 app.get('/restaurants', getRestaurants);
 
